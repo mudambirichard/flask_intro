f7351b337e6507fdb3adf1a79259327499d6c576
mudambi richard <mudarichards@gmail.com>
2018-04-27 17:21:05 +0300

    modify app.py and test.py

--- test.py
+++ test.py
@@ -5,13 +5,13 @@ class FlaskTestCase(unittest.TestCase):
 
 	def test_index(self):
 		tester = app.test_client(self)
-		response = tester.get('/login', content_type='html/text')
+		response = tester.get('/api/v1/auth/login', content_type='html/text')
 		self.assertEqual(response.status_code, 200)
 
     
         def test_login_page_loads(self):
     	    tester = app.test_client(self)
-    	    response = tester.get('/login', content_type='html/text')
+    	    response = tester.get('/api/v1/auth/login', content_type='html/text')
     	    self.assertTrue(b'Please login' in response.data)
 
      ## test login page
@@ -26,7 +26,7 @@ class FlaskTestCase(unittest.TestCase):
     	def test_correct_login(self):
     		tester = app.test_client(self)
     		response = tester.post(
-    			'/login',
+    			'/api/v1/auth/login',
     			data=dict(username="wrong",  password="wrong"),
     			)
     		self.assertIn(b'Invalid credentials. Please try again.', response.data)
@@ -34,29 +34,29 @@ class FlaskTestCase(unittest.TestCase):
 
         def test_register_page_loads(self):
     	    tester = app.test_client(self)
-    	    response = tester.get('/register', content_type='html/text')
+    	    response = tester.get('/api/v1/auth/register', content_type='html/text')
     	    self.assertTrue(b'Please register' in response.data)
 
 
     	def test_post_page_loads(self):
     	    tester = app.test_client(self)
-    	    response = tester.get('/post', content_type='html/text')
+    	    response = tester.get('/api/v1/auth/post', content_type='html/text')
     	    self.assertFalse(b'Please post' in response.data)
 
     		
     	def test_delete_page_loads(self):
             tester = app.test_client(self)
-            response = tester.get('/post', content_type='html/text')
+            response = tester.get('/api/v1/auth/delete', content_type='html/text')
             self.assertFalse(b'Please delete' in response.data)
 
         def test_get_page_loads(self):
             tester = app.test_client(self)
-            response = tester.get('/post', content_type='html/text')
+            response = tester.get('/api/v1/auth/get', content_type='html/text')
             self.assertFalse(b'Please get' in response.data)
 
         def test_put_page_loads(self):
             tester = app.test_client(self)
-            response = tester.put('/post', content_type='html/text')
+            response = tester.put('/api/v1/auth/put', content_type='html/text')
             self.assertFalse(b'Please put ' in response.data)
 
 if __name__ == '__main__':
